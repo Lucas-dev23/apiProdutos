68cfe4b1d7fe12ee4b6dd5f5ab6fe99b1a90ed4c
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2024-06-20 12:50:34 -0300

    first commit

---
+++ src/test/java/br/com/cotiinformartica/ApiProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformartica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
